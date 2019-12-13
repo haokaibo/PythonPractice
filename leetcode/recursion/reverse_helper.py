5a9aa95ce86370ed639bb5b935ff3778be58c937
Kevin Hao <haokaibo@outlook.com>
2019-12-14 00:49:17 +0800

    implement the reverse char arrays.

--- leetcode/recursion/reverse_helper.py
+++ leetcode/recursion/reverse_helper.py
@@ -8,6 +8,21 @@ class StringHelper:
         self.reverse_print(index + 1, str)
         print(str[index])
 
+    def reverse_chars(self, chars):
+        self.reverse_chars_helper(chars, 0, len(chars)-1)
+        return chars
+
+    def reverse_chars_helper(self, chars, begin, end):
+        if begin >= end:
+            return
+        else:
+            self.reverse_chars_helper(chars, begin + 1, end - 1)
+            temp = chars[begin]
+            chars[begin] = chars[end]
+            chars[end] = temp
+
 
 if __name__ == '__main__':
-    StringHelper().print_reversed_string('abc')
+    # StringHelper().print_reversed_string('abc')
+    result = StringHelper().reverse_chars(['a', 'b', 'c'])
+    print(result)
