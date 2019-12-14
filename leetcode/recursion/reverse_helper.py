818385e012bbfeff79926cb907fe7c5d4ccfb9ce
Kevin Hao <haokaibo@outlook.com>
2019-12-14 10:20:45 +0800

    implement the reverse vowels function by loop.

--- leetcode/recursion/reverse_helper.py
+++ leetcode/recursion/reverse_helper.py
@@ -19,9 +19,38 @@ class StringHelper:
 
         helper(0, len(s) - 1)
 
+    def reverseVowels(self, s: str) -> str:
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        _reversed = list(s)
+
+        left = 0
+        right = len(_reversed) - 1
+
+        while left < right:
+            while left < right and _reversed[left].lower() not in vowels:
+                left += 1
+            while left < right and _reversed[right].lower() not in vowels:
+                right -= 1
+            if left < right:
+                _reversed[left], _reversed[right] = _reversed[right], _reversed[left]
+                left += 1
+                right -= 1
+
+        return ''.join(_reversed)
+
 
 if __name__ == '__main__':
+    # test 1
     # StringHelper().print_reversed_string('abc')
-    string = ['a', 'b', 'c']
-    StringHelper().reverseString(string)
-    print(string)
+
+    # test 2
+    # string = ['a', 'b', 'c']
+    # StringHelper().reverseString(string)
+    # print(string)
+
+    # test 3
+    # print(StringHelper().reverseVowels('hello'))
+    with open('test.txt') as f:
+        string = f.read()
+        result = StringHelper().reverseVowels(string)
+        print(result)
