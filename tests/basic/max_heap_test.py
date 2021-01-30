9e27d53a615fa05d3965acbb55cf5afb05f4729b
Kevin Hao <haokaibo@outlook.com>
2021-01-30 20:02:09 +0800

    implement the max_heap class in python.

---
+++ tests/basic/max_heap_test.py
@@ -0,0 +1,10 @@
+from basic.max_heap import MaxHeap
+from unittest import TestCase
+
+
+class MaxHeapTest(TestCase):
+    def test_pop(self):
+        h = MaxHeap(items=[1, 2, 3, 4, 5])
+        print(h)
+        assert (h.pop(), 5)
+        print(h)
