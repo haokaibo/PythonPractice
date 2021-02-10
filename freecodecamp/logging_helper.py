9b1f66972a66695a166fbd2ce0500d02463026c1
Kevin Hao <haokaibo@outlook.com>
2021-02-10 17:54:24 +0800

    implement the logging usecases.

--- freecodecamp/logging_helper.py
+++ freecodecamp/logging_helper.py
@@ -1,4 +1,6 @@
 import logging
+from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
+
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +14,7 @@ file_h = logging.FileHandler('file.log')
 file_h.setLevel(logging.ERROR)
 file_h.setFormatter(formatter)
 
+
 logger.addHandler(stream_h)
 logger.addHandler(file_h)
 
@@ -19,3 +22,16 @@ logger.addHandler(file_h)
 logger.info('Hello Kaibo.')
 logger.warning('this is warning')
 logger.error('this is error')
+
+# rotate file handler
+rotate_file_h =RotatingFileHandler('app.log', maxBytes=2000, backupCount=5)
+logger.addHandler(rotate_file_h)
+for _ in range(10000):
+    logger.error('Hello, Kaibo!')
+
+import time
+rotate_time_h = TimedRotatingFileHandler('timed_test.log', when='s', interval=5, backupCount=5)
+logger.addHandler(rotate_time_h)
+for _ in range(6):
+    logger.info('Hello time log.')
+    time.sleep(5)
--- freecodecamp/loggings.py
+++ freecodecamp/loggings.py
@@ -18,3 +18,18 @@ import logging_helper
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger('simpleExample')
 logging.debug('this is a debug message')
+
+# print exc_info(stack trace)
+try:
+    a=[1,2,3]
+    val = a[4]
+except Exception as e:
+    logging.error(e, exc_info=True)
+
+# trace back
+import traceback
+try:
+    a=[1,2,3]
+    val = a[4]
+except:
+    logging.error(f'This error is {traceback.format_exc()}')
