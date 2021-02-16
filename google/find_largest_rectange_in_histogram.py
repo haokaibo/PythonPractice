42effc691046303ab4337152f55029839f137caf
Kevin Hao <haokaibo@outlook.com>
2021-02-16 20:07:13 +0800

    implement the use case for find largest rectange in histogram.

--- google/find_largest_rectange_in_histogram.py
+++ google/find_largest_rectange_in_histogram.py
@@ -2,8 +2,77 @@
 Given an array which represent the x coordinates for a histogram chart,
 find the largest area in the histogram.
 """
+from collections import deque
 
-arr = [1, 2, 3, 4, 5, 2, 3, 2]
+
+class Stack:
+    def __init__(self):
+        self.deque = deque()
+
+    def push(self, val):
+        self.deque.append(val)
+
+    def pop(self):
+        return self.deque.pop()
+
+    def peek(self):
+        if self.empty():
+            return None
+        return self.deque[-1]
+
+    def empty(self):
+        return len(self.deque) == 0
+
+
+def get_largest_area_in_histogram(arr):
+    """
+    Optimized implementation which took O(n) to implement.
+    """
+    max_area = 0
+    if arr is None or len(arr) == 0:
+        return 0
+
+    stack = Stack()
+
+    for i, val in enumerate(arr):
+        if stack.empty() or arr[stack.peek()] < val:
+            stack.push(i)
+        else:
+            while not stack.empty() and arr[stack.peek()] > val:
+                curr_max_index = stack.pop()
+                width = i - curr_max_index
+                height = arr[curr_max_index]
+                max_area = max(width * height, max_area)
+            stack.push(i)
+
+    while not stack.empty():
+        curr_max_index = stack.pop()
+        if stack.empty():
+            width = len(arr)
+        else:
+            width = len(arr) - stack.peek() - 1
+        height = arr[curr_max_index]
+        max_area = max(width * height, max_area)
+    return max_area
+
+
+arrs = [
+    [1, 2, 3, 4, 5, 5, 3, 2],
+    [1, 1],
+    [2, 1, 2],
+    [1, 2, 1],
+    [1, 2, 2, 1],
+    [1, 3, 3, 1],
+    [1, 3, 1, 1],
+    [1, 3, 5, 5, 4, 10]
+]
+for a in arrs:
+    print(f"{a}.max_area={get_largest_area_in_histogram(a)}")
+
+
+#
+#
+# Bad implementation which takes O(n^2) to finish.
 
 
 def find_largest_area_in_histogram(arr):
@@ -29,5 +98,4 @@ def find_largest_area_in_histogram(arr):
 
     return max_area
 
-
-print(find_largest_area_in_histogram(arr))
+# print(find_largest_area_in_histogram(arr))
