25c2aa751193e80987ba90f9a697723d15dd9840
Kaibo <haokaibo@outlook.com>
2026-01-01 12:11:33 -0800

    Implemented Tree Number Sum problem in AlgoExpert.

---
+++ src/AlgoExpert/three_number_sum.py
@@ -0,0 +1,26 @@
+def threeNumberSum(array, targetSum):
+    # Write your code here.
+    """
+    Optimized Solution 2
+    Time: O(n^2)
+    Space: O(log(n))
+    """
+    triplets = []
+    array.sort()
+
+    for i in range(len(array) - 2):
+        left = i + 1
+        right = len(array) - 1
+
+        while left < right:
+            current_sum = array[i] + array[left] + array[right]
+            if current_sum == targetSum:
+                triplets.append([array[i], array[left], array[right]])
+                left += 1
+                right -= 1
+            elif current_sum > targetSum:
+                right -= 1
+            elif current_sum < targetSum:
+                left += 1
+
+    return triplets
