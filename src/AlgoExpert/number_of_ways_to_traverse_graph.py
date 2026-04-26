ccf80b9339922d4aec50607fd70d35378018033c
Kaibo <haokaibo@outlook.com>
2026-04-25 21:09:51 -0700

    Implemented the numberOfWaysToTraverseGraph problem in AlgoExpert.

---
+++ src/AlgoExpert/number_of_ways_to_traverse_graph.py
@@ -0,0 +1,29 @@
+"""_summary_
+Solution(Time:O(w * h), Space: O(w * h) )
+target: find the ways for left top -> right bottom
+reach the end: height == 0 and right == 0
+action: down(height-1), right(width-1)
+1 1
+1 2
+1 3(1 + 2)
+"""
+
+class Solution:
+
+    @staticmethod
+    def numberOfWaysToTraverseGraph(width, height):
+        # Write your code here.
+        if width == 1 or height == 1:
+            return 1
+            
+        matrix = [[1] * width] * height
+        for r in range(1, height):
+            for c in range(1, width):
+                matrix[r][c] = matrix[r - 1][c] + matrix[r][c - 1]
+        
+        return matrix[height - 1][width - 1]
+
+width = 2
+height = 3
+print(f"numberOfWaysToTraverseGraph for width: {width}, height: {height} -> "
+       f"{Solution.numberOfWaysToTraverseGraph(width, height)}")
